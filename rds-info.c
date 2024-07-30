markhaywood/rds-tools

commit 464bfeefb118f5c5f912f1db42b1e38f680d83b8

rds-info: Userspace change to display congested socket on rds-info -k
Orabug: 34281720

Currently rds-info does not display if a particular socket is congested
or not. With this addition, we aim to add a congested field to the
output of rds-info -k so that we may be able to observe when a
particualr rds-socket is congested.

This is the userspace change required required to display this
information and requires a supported kernel containing the corresponding
kernel changes.

Signed-off-by: Rohit Nair <rohit.sajan.kumar@oracle.com>
Reviewed-by: Gerd Rausch <gerd.rausch@oracle.com>

rds-info.c
@@ -172,6 +172,20 @@ static int get_comm(pid_t pid, char *comm, int sz)
 	return 0;
 }
 
+/*
+ * On a non supported kernel, the value in the field would be set to 0
+ * and a supported kernel returns -1 to indicate a non-congested state.
+ * Thus we use this function to swap the 0 and -1 values to indicate
+ * a non congested state and a non-supported state.
+ */
+static void get_congested(int *congested)
+{
+	if (*congested == 0)
+		*congested = -1;
+	else if (*congested == -1)
+		*congested = 0;
+}
+
 static void print_sockets(void *data, int each, socklen_t len, void *extra,
 			  bool prt_ipv6)
 {
@@ -185,9 +199,9 @@ static void print_sockets(void *data, int each, socklen_t len, void *extra,
 	else
 		prt_width = PRT_IPV4_WIDTH;
 
-	printf("\nRDS Sockets:\n%*s %5s %*s %5s %10s %10s %8s %10s %16s\n",
+	printf("\nRDS Sockets:\n%*s %5s %*s %5s %10s %10s %8s %8s %10s %16s\n",
 	       prt_width, "BoundAddr", "BPort", prt_width, "ConnAddr", "CPort",
-	       "SndBuf", "RcvBuf", "Inode", "Pid", "Comm");
+	       "SndBuf", "RcvBuf", "Inode", "Cong", "Pid", "Comm");
 
 	if (prt_ipv6) {
 		for_each(sk6, data, each, len) {
@@ -198,6 +212,8 @@ static void print_sockets(void *data, int each, socklen_t len, void *extra,
 			       ntohs(sk6.connected_port),
 			       sk6.sndbuf, sk6.rcvbuf,
 			       (unsigned long long)sk6.inum);
+			get_congested(&sk6.cong);
+				printf(" %8d", sk6.cong);
 			if (get_comm(sk6.pid, comm, TASK_COMM_LEN) != -1)
 				printf(" %10u %16s", sk6.pid, comm);
 			printf("\n");
@@ -211,6 +227,8 @@ static void print_sockets(void *data, int each, socklen_t len, void *extra,
 			       ntohs(sk.connected_port),
 			       sk.sndbuf, sk.rcvbuf,
 			       (unsigned long long)sk.inum);
+			get_congested(&sk.cong);
+				printf(" %8d", sk.cong);
 			if (get_comm(sk.pid, comm, TASK_COMM_LEN) != -1)
 				printf(" %10u %16s", sk.pid, comm);
 			printf("\n");

include/rds.h
@@ -247,6 +247,7 @@ struct rds_info_socket {
 	u_int32_t	rcvbuf;
 	u_int64_t	inum;
 	pid_t		pid;
+	int32_t		cong;
 } __attribute__((packed));
 
 struct rds6_info_socket {
@@ -258,6 +259,7 @@ struct rds6_info_socket {
 	uint32_t	rcvbuf;
 	uint64_t	inum;
 	pid_t		pid;
+	int32_t		cong;
 } __attribute__((packed));
 
 struct rds_info_tcp_socket {

rds-info.1
@@ -64,6 +64,10 @@ receiving on the socket, respectively.
 The number of the inode object associated with the socket. Can be used to
 locate the process owning a given socket by searching /proc/*/fd for
 open files referencing a socket with this inode number.
+.It Cong
+This field displays 1 if a rds socket is in a congested state and 0 otherwise.
+Needs kernel support and will display -1 if the the kernel does not support
+this field.
 .It Pid
 The pid of the process that intitially created the socket. Needs kernel
 support to be displayed and will be left blank otherwise.

commit 21555d63f48df98f2210279cc7ca2a48a6ad1a8b

rds-info: Fix congestion field in rds-info -k
Orabug: 36349486

With commit 464bfeef ("rds-info: Userspace change to display congested
socket on rds-info -k") and its kernel part, we received -1 for 'no
congestion' and 1 for 'congestion'. We used to remap the 'no congestion'
to 0 before printing. This was done to work with older kernel which
doesn't send the congestion value to userspace. Now we can handle the
unsupported kernels in user space, by initializing the userspace value
to -1. If older kernel doesn't fill this value, it remains -1 in
userspace.

With this change, we would print -1 for 'no congestion' for few kernels
where commit 978f8509 ("rds: Include congested flag in rds_sock
struct.") is present. In such cases, we have to manually examine if -1
means not supported kernel or no congestion. Going forward, 0 would mean
no congestion, 1 would mean congestion and -1 would mean non-supported
kernel.

Fixes: 464bfeef ("rds-info: Userspace change to display congested socket
on rds-info -k")
Signed-off-by: Juan Garcia <juan.ma.garcia@oracle.com>
Reviewed-by: Mark Haywood <mark.haywood@oracle.com>

rds-info.c
@@ -204,21 +204,6 @@ static int get_comm(pid_t pid, char *comm, int sz)
 	return 0;
 }
 
-/*
- * On a non supported kernel, the value in the field would be set to 0
- * and a supported kernel returns -1 to indicate a non-congested state.
- * Thus we use this function to swap the 0 and -1 values to indicate
- * a non congested state and a non-supported state.
- */
-static int get_congested(int congested)
-{
-	if (congested == 0)
-		return -1;
-	else if (congested == -1)
-		return 0;
-        return congested;
-}
-
 static void print_sockets(void *data, int each, socklen_t len, void *extra,
 			  bool prt_ipv6)
 {
@@ -232,38 +217,37 @@ static void print_sockets(void *data, int each, socklen_t len, void *extra,
 	else
 		prt_width = PRT_IPV4_WIDTH;
 
-	printf("\nRDS Sockets:\n%*s %5s %*s %5s %10s %10s %8s %16s %8s %10s %16s\n",
+	printf("\nRDS Sockets:\n%*s %5s %*s %5s %10s %10s %10s %16s %8s %10s %16s\n",
 	       prt_width, "BoundAddr", "BPort", prt_width, "ConnAddr", "CPort",
 	       "SndBuf", "RcvBuf", "Inode", "TransName", "Cong", "Pid", "Comm");
 
+	/* Initialize for compatibility with older kernels. */
+	sk6.cong = -1;
+	sk.cong = -1;
 	if (prt_ipv6) {
 		for_each(sk6, data, each, len) {
-			printf("%*s %5u %*s %5u %10u %10u %8llu %16s",
+			printf("%*s %5u %*s %5u %10u %10u %10llu %16s %8d",
 			       prt_width, ipaddr(&sk6.bound_addr, prt_ipv6),
 			       ntohs(sk6.bound_port),
 			       prt_width, ipaddr(&sk6.connected_addr, prt_ipv6),
 			       ntohs(sk6.connected_port),
 			       sk6.sndbuf, sk6.rcvbuf,
 			       (unsigned long long)sk6.inum,
-			       sk6.t_name);
-			sk6.cong = get_congested(sk6.cong);
-				printf(" %8d", sk6.cong);
+			       sk6.t_name, sk6.cong);
 			if (get_comm(sk6.pid, comm, TASK_COMM_LEN) != -1)
 				printf(" %10u %16s", sk6.pid, comm);
 			printf("\n");
 		}
 	} else {
 		for_each(sk, data, each, len) {
-			printf("%*s %5u %*s %5u %10u %10u %8llu %16s",
+			printf("%*s %5u %*s %5u %10u %10u %10llu %16s %8d",
 			       prt_width, ipaddr(&sk.bound_addr, prt_ipv6),
 			       ntohs(sk.bound_port),
 			       prt_width, ipaddr(&sk.connected_addr, prt_ipv6),
 			       ntohs(sk.connected_port),
 			       sk.sndbuf, sk.rcvbuf,
 			       (unsigned long long)sk.inum,
-			       sk.t_name);
-			sk.cong = get_congested(sk.cong);
-				printf(" %8d", sk.cong);
+			       sk.t_name, sk.cong);
 			if (get_comm(sk.pid, comm, TASK_COMM_LEN) != -1)
 				printf(" %10u %16s", sk.pid, comm);
 			printf("\n");